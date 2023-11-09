01846fa080736054bfe95d93c90ec3aef6e2d653
Timur Satiev <60050123+tipask245@users.noreply.github.com>
2023-11-09 13:51:03 +0000

    add pagekite and cors

--- src/routers/trains.py
+++ src/routers/trains.py
@@ -1,8 +1,14 @@
 from flask import Blueprint
-
+from flask_cors import cross_origin
 from services.trains_service import TrainsService
 
 
-trains = Blueprint('trains', __name__)
+trains = Blueprint('trains', __name__, url_prefix='/trains')
 
 trains_service = TrainsService()
+
+
+@trains.route('/', methods=["get"])
+@cross_origin()
+def get_trains():
+    return 'ok', 200
